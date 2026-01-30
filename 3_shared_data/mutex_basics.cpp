9344c9444c754841c25e7829df7dc40b3c289284
aitoAarni <aarni.rechardt@gmail.com>
2026-01-30 23:06:53 +0200

    practice mutexes

---
+++ 3_shared_data/mutex_basics.cpp
@@ -0,0 +1,34 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <functional>  // for std::ref
+#include <array>
+
+void f_lock_guard_version(int number, std::mutex& mutex) {
+    std::lock_guard<std::mutex> lg {mutex};
+    std::cout << "In thread: " << std::this_thread::get_id() << "\n"
+        << "t num: " << number << "\n";
+}
+
+void f_mutex_version(int number, std::mutex& mutex) {
+    mutex.lock();
+    std::cout << "In thread: " << std::this_thread::get_id() << "\n"
+        << "t num: " << number << "\n";
+    mutex.unlock();
+}
+
+int main() {
+    std::mutex print_mutex;
+    int thread_num {static_cast<int>(std::thread::hardware_concurrency())};
+    std::array<std::thread, 32> threads;
+
+    for (int t = 1; t < thread_num; t++) {
+        threads[t] = std::thread(f_lock_guard_version, t, std::ref(print_mutex));
+    }
+
+    for (int t = 1; t < thread_num; t++) {
+        threads[t].join();
+    }
+
+    return 0;
+}
