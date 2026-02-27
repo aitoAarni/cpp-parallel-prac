f6a63a9f8a68a3825c9d160c818023a5b579c517
aitoAarni <aarni.rechardt@gmail.com>
2026-02-27 23:30:59 +0200

    add file

--- 4_synchronizing_concurrent_operations/future_async.cpp
+++ 4_synchronizing_concurrent_operations/future_async.cpp
@@ -9,9 +9,13 @@ std::string get_additon_string(const int a, const int b) {
 
 int main() {
     // std::launch::async  makes it launch immediately as a thread
-    // it also loses refs, since values are copied to the async constructor
+    // std::launch::deferred makes it run when get() or wait() memb. funcs are called
+    // it also loses refs (like thread), since values are copied to the async constructor
     // like with threads
-    auto maths = std::async(std::launch::async, get_additon_string, 6, 7);
+    std::future<std::string> maths = std::async(
+        std::launch::async, get_additon_string, 6, 7
+    );
+
     std::cout << maths.get() << "\n";
     return 0;
 }
