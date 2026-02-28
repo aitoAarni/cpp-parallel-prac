f3e6dbc0c8e5f9cc524c85b6df1b7c99cc8ef786
aitoAarni <aarni.rechardt@gmail.com>
2026-02-28 23:51:11 +0200

    update example

--- 4_synchronizing_concurrent_operations/promise_future.cpp
+++ 4_synchronizing_concurrent_operations/promise_future.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <future>
 #include <string>
+#include <chrono>
 
 void set_data_thread(std::promise<std::string>& s) {
     std::string input {};
@@ -16,7 +17,12 @@ int main() {
 
     std::jthread proimse_thread(set_data_thread, std::ref(promise_string));
 
-    future_string.wait(); // not necessary
+    while (future_string.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "waiting for input..." << "\n";
+    }
+
+    future_string.wait(); // not necessary since we already confirmed before it is ready
     std::cout << "You wrote: " << future_string.get() << "\n";
     return 0;
 }
